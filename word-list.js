adce8cf696b859749d31838f5a94aed60eacfdec
David <118802927+DavidDavid122@users.noreply.github.com>
2023-07-29 11:20:58 +0300

    initial commit

---
+++ word-list.js
@@ -0,0 +1,54 @@
+const wordList = [
+    {
+        word: "guitar",
+        hint: "A musical instrument with strings."
+    },
+    {
+        word: "oxygen",
+        hint: "A colorless, odorless gas essential for life."
+    },
+    {
+        word: "mountain",
+        hint: "A large natural elevation of the Earth's surface,"
+    },
+    {
+        word: "painting",
+        hint: "An art form using colors on a surface to create images or expression."
+    },
+    {
+        word: "astronomy",
+        hint: "The scientific study of celestial objects and phenomena."
+    },
+    {
+        word: "butterfly",
+        hint: "An insect with colorful wings and a slender body."
+    },
+    {
+        word: "jazz",
+        hint: "A genre of music characterized by improvisation and syncopation."
+    },
+    {
+        word: "camera",
+        hint: "A device used to capture and record images or videos."
+    },
+    {
+        word: "chocolate",
+        hint: "A sweet treat made from cocoa beans."
+    },
+    {
+        word: "adventure",
+        hint: "An exciting or daring experience."
+    }, 
+    {
+        word: "sunset",
+        hint: "The daily disappearance of the sun below the horizon."
+    },
+    {
+        word: "diamond",
+        hint: "A precious gemstone known for its brilliance and hardness."
+    },
+    {
+        word: "bicycle",
+        hint: "A human-powered vehicle with two wheels."
+    }
+];
